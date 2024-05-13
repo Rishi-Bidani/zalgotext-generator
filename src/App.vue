934e8672ebef6b885b18ccd58c303699734dc3dd
Rishi <64310471+Rishi-Bidani@users.noreply.github.com>
2024-05-13 22:23:27 +1000

    initial commit. pretty much ready

---
+++ src/App.vue
@@ -0,0 +1,148 @@
+<template>
+    <div id="app">
+        <h1>Random Diacritics | Zalgotext</h1>
+        <main>
+            <textarea name="inputtext" cols="30" rows="10" v-model="inputText"></textarea>
+            <article class="container">
+                <div class="output">
+                    {{ output }}
+                </div>
+                <button class="clipboard" @click="copyToClipboard">📋 Copy to clipboard</button>
+            </article>
+        </main>
+        <section class="selection">
+            <h2>Crazy Selector</h2>
+            <input type="range" min="0" max="50" step="1" v-model="rangeValue" />
+        </section>
+        <Instructions />
+    </div>
+</template>
+
+<script lang="ts" setup>
+import Instructions from "./components/Instructions.vue";
+import { ref, watch } from "vue";
+
+const inputText = ref("");
+const output = ref("");
+const rangeValue = ref(1);
+
+// prettier-ignore
+const diacriticsUnicdeNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
+const diacriticsUnicodeBase = ["u030", "u031", "u032", "u033", "u034", "u035", "u036"];
+
+function generateDiacritics(char: string, number: number) {
+    // for each character in the input text generate x random diacritics
+    let diacriticChar = "";
+    for (let i = 0; i < number; i++) {
+        const diacritic =
+            diacriticsUnicodeBase[Math.floor(Math.random() * diacriticsUnicodeBase.length)] +
+            diacriticsUnicdeNumbers[Math.floor(Math.random() * diacriticsUnicdeNumbers.length)];
+        diacriticChar += `\\` + diacritic;
+    }
+    return char + diacriticChar;
+}
+
+function showOutput(inp: string, crazy: number) {
+    let finalString = "";
+    for (let i = 0; i < inputText.value.length; i++) {
+        const char = inp[i];
+
+        finalString += generateDiacritics(char, crazy);
+    }
+    // make it render the html
+    output.value = finalString.replace(/\\u([a-f0-9]{4})/gi, (n, hex) => {
+        return String.fromCharCode(parseInt(hex, 16));
+    });
+}
+
+function copyToClipboard() {
+    navigator.clipboard.writeText(output.value);
+}
+
+watch(inputText, () => {
+    showOutput(inputText.value, rangeValue.value);
+});
+
+watch(rangeValue, () => {
+    showOutput(inputText.value, rangeValue.value);
+});
+</script>
+
+<style>
+:root {
+    --bg-color: hsl(0, 0%, 10%);
+    color-scheme: light dark;
+}
+
+* {
+    margin: 0;
+    padding: 0;
+}
+
+body {
+    background-color: var(--bg-color);
+    color: white;
+    font-family: "Arial", sans-serif;
+}
+
+#app {
+    display: flex;
+    flex-direction: column;
+    margin-inline: auto;
+    max-width: 80rem;
+}
+
+h1 {
+    padding-block: 1rem;
+}
+
+main {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 1rem;
+}
+
+main textarea,
+main .container {
+    flex: 1;
+    min-width: 250px;
+    font-size: 1.5rem;
+    /* height: 15rem; */
+    height: auto;
+}
+
+main textarea {
+    padding: 1em;
+}
+
+.container {
+    display: flex;
+    flex-flow: column;
+    gap: 1rem;
+}
+
+section {
+    padding-block: 1rem;
+}
+
+h2 {
+    padding-block: 0.5rem;
+}
+
+textarea {
+    resize: vertical;
+}
+
+.output {
+    border: 1px solid black;
+    min-width: 250px;
+    padding: 1em;
+    font-size: 1.5rem;
+    height: 100%;
+}
+
+.selection {
+    display: flex;
+    flex-direction: column;
+}
+</style>
---
+++ src/components/Instructions.vue
@@ -0,0 +1,22 @@
+<template>
+    <section>
+        <h2>Instructions</h2>
+        <p>
+            Write some text in the input box and it will generate random diacritics for each
+            character in the text.
+        </p>
+        <p>
+            The diacritics are generated randomly and can be any of the following:
+            <code>u030</code>, <code>u031</code>, <code>u032</code>, <code>u033</code>,
+            <code>u034</code>, <code>u035</code>, <code>u036</code> followed by a number from 0 to 9
+            or a letter from A to F.
+        </p>
+        <p>The output is in the form of a zalgotext, which is a text with random diacritics.</p>
+    </section>
+</template>
+<script setup lang="ts"></script>
+<style scoped>
+section p {
+    line-height: 1.5rem;
+}
+</style>
